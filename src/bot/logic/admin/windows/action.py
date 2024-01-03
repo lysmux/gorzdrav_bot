f3b287a28b0594c53f46092223e6278f84348feb
KllRaz <kllraz@yandex.ru>
2024-01-03 19:51:58 +0300

    Add menu button to admin panel

--- src/bot/logic/admin/windows/action.py
+++ src/bot/logic/admin/windows/action.py
@@ -3,6 +3,7 @@ from aiogram_dialog.widgets.kbd import SwitchTo
 from aiogram_dialog.widgets.text import Const, Jinja
 
 from src.bot.logic.admin.states import AdminStates
+from src.bot.utils.buttons import get_menu_button
 
 WINDOW_NAME = "admin"
 STATISTICS_BTN_ID = f"{WINDOW_NAME}_statistics_btn"
@@ -14,5 +15,6 @@ window = Window(
         id=STATISTICS_BTN_ID,
         state=AdminStates.statistics
     ),
+    get_menu_button(),
     state=AdminStates.action
 )
